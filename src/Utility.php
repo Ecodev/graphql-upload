eb09d418c0dac6543e5bcacf17cab365a7abaa10
Sylvain Tissot <sylvain.tissot@ecodev.ch>
2020-06-19 18:42:51 +0200

    Add Utility::modelToId

--- src/Utility.php
+++ src/Utility.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Ecodev\Felix;
 
+use Ecodev\Felix\Model\Model;
 use GraphQL\Doctrine\Definition\EntityID;
 use ReflectionClass;
 
@@ -52,6 +53,28 @@ abstract class Utility
         foreach ($data as &$value) {
             if ($value instanceof EntityID) {
                 $value = $value->getEntity();
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * Replace object by their ID in the array and don't touch other values
+     *
+     * Support both AbstractModel and EntityID.
+     *
+     * @param array $data mix of objects and scalar values
+     */
+    public static function modelToId(?array $data): ?array
+    {
+        if ($data === null) {
+            return null;
+        }
+
+        foreach ($data as &$value) {
+            if ($value instanceof Model || $value instanceof EntityID) {
+                $value = $value->getId();
             }
         }
 
--- tests/UtilityTest.php
+++ tests/UtilityTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace EcodevTests\Felix;
 
+use Ecodev\Felix\Model\Model;
 use Ecodev\Felix\Utility;
 use EcodevTests\Felix\Blog\Model\User;
 use GraphQL\Doctrine\Definition\EntityID;
@@ -17,7 +18,7 @@ final class UtilityTest extends \PHPUnit\Framework\TestCase
         self::assertSame('User', Utility::getShortClassName(User::class));
     }
 
-    public function testModelToId(): void
+    public function testEntityIdToModel(): void
     {
         $input = [
             3 => new stdClass(),
@@ -43,5 +44,25 @@ final class UtilityTest extends \PHPUnit\Framework\TestCase
         self::assertSame($expected, $actual, 'keys and non model values should be preserved');
         self::assertNull(Utility::entityIdToModel(null));
         self::assertSame([], Utility::entityIdToModel([]));
+    }
+
+    public function testModelToId(): void
+    {
+        $input = [
+            'entity' => new class() implements Model {
+                public function getId(): ?int
+                {
+                    return 123456;
+                }
+            },
+            4 => 1,
+        ];
+
+        $actual = Utility::modelToId($input);
+
+        $expected = $input;
+        $expected['entity'] = 123456;
+
+        self::assertSame($expected, $actual, 'models must be replaced by their ids, other values should be preserved');
     }
 }
